2a469292439b5a39758c9440a3b0feb1dd33f3ab
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 23:55:35 +0530

    Create p_7.c

---
+++ pattern/p_7.c
@@ -0,0 +1,19 @@
+//pattern:
+//    1
+//   12
+//  123
+// 1234
+#include <stdio.h>
+void mian()
+{
+  int i,j,k;
+  int n=5; // size of the pattern
+  for(i=1;i<=n;i++)
+  {
+    for(k=n-i+1;k>=1;k--)
+      printf(" ");
+    for(j=1;j<=i;j++)
+      printf("%d", j);
+   printf("\n");
+   }
+}
