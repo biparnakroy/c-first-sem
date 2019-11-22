d9d2cca9108a2a643f807829393b2e5f736a15dc
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 21:49:56 +0530

    Create lp_5.c

---
+++ loops/lp_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	int n=0;
+	printf("ENTER THE NUMBER TO FING THE SUM\n");
+	scanf("%d", &n);
+	int i=0;
+	int sum=0;
+	for(i=1;i<=n;i++)
+	{
+		sum+=(i*(i+1));
+	}
+	printf("THE SUM =%d", sum);
+
+}
