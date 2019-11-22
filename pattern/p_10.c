87f93dc47a42d91bfad4b5ceaa803c4e09b71a2a
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-23 00:24:03 +0530

    Create p_10.c

---
+++ pattern/p_10.c
@@ -0,0 +1,19 @@
+//pattern 10:
+//     *
+//    * *
+//   * * *
+//  * * * *
+#include <stdio.h>
+void main()
+{
+  int i,j,k;
+  int n =5; //size of the pattern
+  for(i=1;i<=n;i++)
+  {
+    for(k=n-i+1;k>=1;k--)
+      printf(" ");
+     for(j=1;j<=i;j++)
+      printf("%d ", j);
+     printf("\n");
+   }
+ }
