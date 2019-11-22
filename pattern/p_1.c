fd49932437147fb64b5253d5766a021734da803f
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 23:30:23 +0530

    Create p_1.c

---
+++ pattern/p_1.c
@@ -0,0 +1,20 @@
+//pattern 1:
+//*
+//* *
+//* * *
+//* * * *
+#include <stdio.h>
+void main()
+{
+  int i,j;
+  int n =5 // the size of the pattern
+  for(i=i;i<=n;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("*");
+     }
+      printf("\n");
+   }
+}
+   
