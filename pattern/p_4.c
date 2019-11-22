09c3345ef6d563f916b14f2e94c734dcc48f2e0f
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 23:48:04 +0530

    Update p_4.c

--- pattern/p_4.c
+++ pattern/p_4.c
@@ -3,3 +3,17 @@
 //12
 //123
 //1234
+#include <stdio.h>
+void main()
+{
+  int i,j;
+  int n =5 // the size of the pattern
+  for(i=i;i<=n;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d", j);
+     }
+      printf("\n");
+   }
+}
