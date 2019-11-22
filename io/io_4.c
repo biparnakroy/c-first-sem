24b4bcb6e7586f516bd958c9fc6e1dac739f5906
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 22:21:23 +0530

    Update io_4.c

--- io/io_4.c
+++ io/io_4.c
@@ -1,3 +1,6 @@
+//Quetsion 4:
+//An integer is entered as an input through the keyboard. Write a program to find out whether it is
+//an odd number or an even number.
 #include <stdio.h>
 void main()
 {
