856dd67b01fec9cc6831277964c90a929879c12d
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 22:34:24 +0530

    Create io_7.c

---
+++ io/io_7.c
@@ -0,0 +1,8 @@
+//Question 7:
+//Accept three integer numbers and find their average. Next display which numbers are below and
+//above the average value.
+#inluced <stdio.h>
+void main()
+{
+  int a,b,c;
+  }
