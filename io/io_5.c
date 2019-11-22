e61a0bf59444947506d7cd69d560e8432a19402b
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 22:22:17 +0530

    Update io_5.c

--- io/io_5.c
+++ io/io_5.c
@@ -1,3 +1,7 @@
+//Question 5:
+//Input two integer numbers and divide the larger number by the smaller one. Then display the
+//result using printf() function as a fractional number first and then as a real valued number.
+//(Example: 9 divided by 5 shall yield “ 4/5” and “1.8” respectively. )
 #include <stdio.h>
 void main()
 {
