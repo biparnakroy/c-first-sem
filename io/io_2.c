6be4f5fc3c7e01b082cbd3894ae4cd8eb69319f7
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 22:18:47 +0530

    Update io_2.c

--- io/io_2.c
+++ io/io_2.c
@@ -1,3 +1,7 @@
+//Question 2"
+//The length and breadth of a rectangle and radius of a circle are input through the keyboard. Write
+//a program to calculate the area and perimeter of the rectangle, and the area and circumference of
+//the circle.
 #include <stdio.h>
 void main()
 {
