1bfaee20f68f5c2478c52cbf2d9d10c30333ade9
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-22 23:49:52 +0530

    Create p_8.c

---
+++ pattern/p_8.c
@@ -0,0 +1,19 @@
+//pattern 6:
+//1
+//22
+//333
+//4444
+#include <stdio.h>
+void main()
+{
+  int i,j;
+  int n =5 // the size of the pattern
+  for(i=i;i<=n;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d", i);
+     }
+      printf("\n");
+   }
+}
