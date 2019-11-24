9f780f2cdfba6563bcd2a1f2e4238fecaf57c82d
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-25 00:17:09 +0530

    Update a_5_slection_sort.c

--- array/a_5_slection_sort.c
+++ array/a_5_slection_sort.c
@@ -3,5 +3,28 @@
 #include <stdio.h>
 void main()
 {
-  // init
+  int a[10];
+  int i,j,k, temp=0;
+  printf("ENTER 10 ELEMNTS:");
+  for(i=0;i<10;i++)
+  {
+    scanf("%d", &a[i]);
+  }
+    //selection sort
+    for(i=0;i<9;i++)
+    {
+      int max=i;
+      for(j=i+1;j<10;j++)
+      {
+        if(a[j]<a[max]){
+          max=j;
+        }
+      }
+      temp=a[i];
+      a[i] =a[max];
+      a[max]=temp;
+    }
+    for(i=0;i<10;i++)
+      printf("%d ", a[i]);
+  }
   
