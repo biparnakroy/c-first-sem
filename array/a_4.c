6653c8d5842d1cb9b1306275553b17c7ccab40db
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-11-24 18:43:31 +0530

    Update a_4.c

--- array/a_4.c
+++ array/a_4.c
@@ -1,3 +1,25 @@
 //Question 4:
 // Find the range of the set of numbers entered by the user 
 // range in the diff b/w the max & min
+#include <stdio.h>
+void main()
+{
+  int max=0, min=2147485647;
+  printf("ENTER THE SIZE OF ARRAY:\n");
+  int n =0;
+  scanf("%d", &n);
+  int a[n];
+  for(int i =0;i<n;i++)
+  {
+    scanf("ENTER THE ELEMENTS: %d", &a[i]);
+  }
+   for(int i =0;i<n;i++)
+  {
+    if(a[i]>max)
+      max=a[i];
+     if(a[i]<min)
+       min=a[i];
+  }
+  int range= max-min;
+  printf("THE RANGE OF THE ARRAY: %d", range);
+}
