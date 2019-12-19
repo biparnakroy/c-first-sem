7db3db4dabb0e7ceb1a44077e1a57eb15f9ea905
Biparnak Roy <54219771+biparnakroy@users.noreply.github.com>
2019-12-20 00:57:36 +0530

    Update newBckDiff.c

--- nm/interpolation/newBckDiff.c
+++ nm/interpolation/newBckDiff.c
@@ -1,46 +1,61 @@
-#include <stdio.h>
-//interpolation
-//will only work for 4 data
-void main()
-{
-	//no of data entries
-	int n;
-	scanf("%d",&n);
-	//input of data
-	double  _x[n],_y[n];
-	for(int i=0;i<n;i++)
-	{
-		printf("x%d:", (i+1));
-		scanf("%lf",&_x[i]);
-	
-		printf("y%d:", (i+1));
-		scanf("%lf",&_y[i]);
-	}
-	//value to interpolate
-	 double x;
-	scanf("%lf", &x);
-	//newton backward
-		double dy[n-1];
-		double ddy[n-2];
-		double dddy[n-3];
-		// table
-		for(int i=0;i<n-1;i++)
-		{	
-			dy[i]=_y[i+1]-_y[i];
-		}
-		for(int i=0;i<n-2;i++)
-		{
-			ddy[i]=dy[i+1]-dy[i];
-		}
-		for(int i=0;i<n-2;i++)
-		{
-			dddy[i]=ddy[i+1]-ddy[i];
-		}
-		//value compute
-		double p;
-		p=(x - _x[n-1])/(_x[1]-_x[0]);
-		double y;
-		y= _y[n-1] + (p* dy[n-2]) + ( ((p*(p+1))/2)*ddy[n-3] )+ ( ((p*(p+1)*(p+2))/6)*dddy[n-4] );
-		// print
-		printf("y=%lf", y);
-}
+// newton backward interpolation 
+#include <stdio.h> 
+
+// Calculation of u mentioned in formula 
+float u_cal(float u, int n) 
+{ 
+	float temp = u; 
+	for (int i = 1; i < n; i++) 
+		temp = temp * (u + i); 
+	return temp; 
+} 
+
+// Calculating factorial of given n 
+int fact(int n) 
+{ 
+	int f = 1; 
+	for (int i = 2; i <= n; i++) 
+		f *= i; 
+	return f; 
+} 
+
+void main() 
+{ 
+	// number of values given 
+	int n = 5; 
+	float x[] = { 1891, 1901, 1911, 1921, 1931 }; 
+					
+	// y[][] is used for difference 
+	// table and y[][0] used for input 
+	float y[n][n]; 
+	y[0][0] = 46; 
+	y[1][0] = 66; 
+	y[2][0] = 81; 
+	y[3][0] = 93; 
+	y[4][0] = 101; 
+
+	// Calculating the backward difference table 
+	for (int i = 1; i < n; i++) { 
+		for (int j = n - 1; j >= i; j--) 
+			y[j][i] = y[j][i - 1] - y[j - 1][i - 1]; 
+	} 
+
+	// Displaying the backward difference table 
+	for (int i = 0; i < n; i++) { 
+		for (int j = 0; j <= i; j++) 
+			printf("%f\t",y[i][j]);
+		printf("\n");
+	} 
+
+	// Value to interpolate at 
+	float value = 1925; 
+
+	// Initializing u and sum 
+	float sum = y[n - 1][0]; 
+	float u = (value - x[n - 1]) / (x[1] - x[0]); 
+	for (int i = 1; i < n; i++) { 
+		sum = sum + (u_cal(u, i) * y[n - 1][i]) / fact(i); 
+	} 
+
+	printf("%f",sum);
+} 
